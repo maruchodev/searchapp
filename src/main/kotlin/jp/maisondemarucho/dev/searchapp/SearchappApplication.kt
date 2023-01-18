b1c7bf70b79a96b4dba9040f3c23cd521b94b3b6
mm <marucho.dev@gmail.com>
2023-01-18 21:14:44 +0900

    main

---
+++ src/main/kotlin/jp/maisondemarucho/dev/searchapp/SearchappApplication.kt
@@ -0,0 +1,11 @@
+package jp.maisondemarucho.dev.searchapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchappApplication
+
+fun main(args: Array<String>) {
+	runApplication<SearchappApplication>(*args)
+}
---
+++ src/test/kotlin/jp/maisondemarucho/dev/searchapp/SearchappApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.maisondemarucho.dev.searchapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
